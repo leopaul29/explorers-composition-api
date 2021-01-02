44a1c256203393d8533312cee1a0abc33f679a5e
Leo-paul <leopaulmartin@hotmail.fr>
2021-01-02 23:08:30 +0100

    refactor: move method to setup in app

--- src/App.vue
+++ src/App.vue
@@ -60,28 +60,31 @@ export default {
       ]
     })
 
-    return {
-      ...toRefs(state)
+    const registerSelection = gameId => {
+      state.activeScreen = gameId
     }
-  },
-  methods: {
-    registerSelection(gameId) {
-      this.activeScreen = gameId
-    },
-    restartGame() {
-      this.miniGames.forEach(miniGame => {
+    const restartGame = () => {
+      state.miniGames.forEach(miniGame => {
         miniGame.complete = false
       })
 
-      this.activeScreen = 'Not Started'
-    },
-    startGame() {
-      this.activeScreen = 'Home'
-    },
-    updateMiniGame(id) {
-      const miniGame = this.miniGames.find(miniGame => miniGame.id === id)
+      state.activeScreen = 'Not Started'
+    }
+    const startGame = () => {
+      state.activeScreen = 'Home'
+    }
+    const updateMiniGame = id => {
+      const miniGame = state.miniGames.find(miniGame => miniGame.id === id)
 
       miniGame.complete = true
+    }
+
+    return {
+      ...toRefs(state),
+      updateMiniGame,
+      startGame,
+      restartGame,
+      registerSelection
     }
   },
   watch: {
